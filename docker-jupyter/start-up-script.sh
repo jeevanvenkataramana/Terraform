60f74c9f319d23b985ec84280584dfefe7841b01
Ubuntu <ubuntu@ip-172-31-8-122.us-east-2.compute.internal>
2019-03-03 13:24:30 +0000

    docker and jupyter

---
+++ docker-jupyter/start-up-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install docker
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+# Pull and run docker image
+docker run -d -p 80:8888 --name demo jupyter/base-notebook start-notebook.sh --NotebookApp.token=
